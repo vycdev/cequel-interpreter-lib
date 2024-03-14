4bcf8b03a025640110cfe3a9740963570d124410
Sandu Victor <vyctor661@gmail.com>
2024-03-14 16:34:09 +0200

    chore: added parsing exception class

---
+++ Parser/ParsingException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter_lib.Parser
+{
+    internal class ParsingException : Exception
+    {
+        public ParsingException()
+        {
+        }
+
+        public ParsingException(string? message) : base(message)
+        {
+        }
+
+        public ParsingException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ParsingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
