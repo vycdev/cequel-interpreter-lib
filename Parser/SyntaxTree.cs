0a795103ee8e44ab3213d2b1286b1a09026b8700
Sandu Victor <vyctor661@gmail.com>
2024-03-14 16:53:21 +0200

    feat: added SyntaxTree class and Node class for the syntax tree

---
+++ Parser/SyntaxTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter_lib.Parser
+{
+    internal class SyntaxTree
+    {
+        private Node _root = new();
+
+        public void Add(Node node)
+        {
+            _root.Add(node);
+        }
+
+        public void Add(List<Node> nodes)
+        {
+            _root.Add(nodes);
+        }
+    }
+
+    internal class Node
+    {
+        private List<Node> nodes = new();
+
+        #region Constructors
+        public Node() { }
+ 
+        public Node(Node node)
+        {
+            nodes.Add(node);
+        }
+
+        public Node(List<Node> nodes)
+        {
+            this.nodes = nodes;
+        }
+
+        #endregion
+
+        public void Add(Node node)
+        {
+            nodes.Add(node);
+        }
+
+        public void Add(List<Node> nodes)
+        {
+            this.nodes.AddRange(nodes);
+        }
+    }
+}
