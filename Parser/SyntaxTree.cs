7f89ca9be57ad09458a8011290402245dde1e6f9
Sandu Victor <vyctor661@gmail.com>
2024-03-14 16:55:08 +0200

    chore: removed the SyntaxTree class

--- Parser/SyntaxTree.cs
+++ Parser/SyntaxTree.cs
@@ -6,21 +6,6 @@ using System.Threading.Tasks;
 
 namespace Interpreter_lib.Parser
 {
-    internal class SyntaxTree
-    {
-        private Node _root = new();
-
-        public void Add(Node node)
-        {
-            _root.Add(node);
-        }
-
-        public void Add(List<Node> nodes)
-        {
-            _root.Add(nodes);
-        }
-    }
-
     internal class Node
     {
         private List<Node> nodes = new();
