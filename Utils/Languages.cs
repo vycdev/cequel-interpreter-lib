2c3ee27cd4a953cc1e4786191e7dcbc109439921
Sandu Victor <vyctor661@gmail.com>
2024-06-24 13:28:10 +0300

    feat: exercises page with dummy data

--- Utils/Languages.cs
+++ Utils/Languages.cs
@@ -1,9 +1,4 @@
 ﻿using Interpreter_lib.Tokenizer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Interpreter_lib.Utils;
 
